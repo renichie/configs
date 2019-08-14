89c737955d43a4d064bb2228027d621bc840eced
David Eichiner <david.eichiner@donat-it.de>
2019-08-14 13:22:27 +0200

    htop window zu tmux session hinzugefuegt

--- .tmux_start_zax_session.sh
+++ .tmux_start_zax_session.sh
@@ -3,6 +3,7 @@ session="zax"
 window0="workspace"
 window1="zambe-test_log"
 window2="zambe_log"
+window3="htop"
 
 tmux start-server
 tmux new-session -s $session -n $window0\; \
@@ -11,6 +12,7 @@ tmux new-session -s $session -n $window0\; \
 	select-pane -t 0 \; \
 	new-window -n $window1 \; select-window -t $window1 \; send-keys "docker logs -t -f zambe-test" C-m \; \
 	new-window -n $window2 \; select-window -t $window2 \; send-keys "docker logs -t -f zambe" C-m \; \
+	new-window -n $window3 \; select-window -t $window3 \; send-keys "htop" C-m \; \
 	select-window -t $window0 \; select-pane -t 1 \; send-keys "ls" C-m \; \
 	select-window -t $window0 \; select-pane -t 2 \; send-keys "echo \"hier koennte ihr befehl stehen\"" C-m \; \
 	select-window -t $window0 \; select-pane -t 0 \;
