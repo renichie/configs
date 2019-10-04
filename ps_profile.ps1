c6f657234241d8d3c939a6b623068f8d8538cbf1
David Eichiner <david.eichiner@donat-it.de>
2019-10-04 08:26:07 +0200

    adding git aliases

--- ps_profile.ps1
+++ ps_profile.ps1
@@ -1,11 +1,16 @@
-######################################## VARIABLES #######################################
+###################################################################################################
+######################################## VARIABLES ################################################
+###################################################################################################
 $KEYFILE_ZAX = "$HOME\.ssh\zax_sshkey"
 $USR_ZAX = "eichinda"
 $CONFIG_DIR = "$HOME/workspace/configs"
-$STARTING_DIR = "$HOME/workspace"
+$STARTING_DIR = "$HOME"
 
+set-location $STARTING_DIR
 
-################################################## Prompt ########################################################################
+###################################################################################################
+################################################## Prompt #########################################
+###################################################################################################
 Import-Module Posh-Git
 $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
 if ($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) { $Script:IsAdmin = $true }
@@ -37,8 +42,9 @@ function prompt {
     }
 	' '
 }
-
-################################################## Readline options ########################################################################
+###################################################################################################
+########################################## Readline options #######################################
+###################################################################################################
 ## Tab completion
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
 Set-PSReadlineOption -ShowToolTips
@@ -53,25 +59,26 @@ Set-PSReadlineOption -ShowToolTips
 #Set-PSReadlineOption -TokenKind String -ForegroundColor DarkGray
 #Set-PSReadlineOption -TokenKind Type -ForegroundColor DarkCyan
  
-# AdvancedHistory
+#AdvancedHistory
 Import-Module AdvancedHistory
 Enable-AdvancedHistory
 
 Set-PSReadlineKeyHandler -Key Ctrl+d -Function DeleteCharOrExit
 
-# set directory to start
-set-location $STARTING_DIR
-
-######################################## ALIASES #######################################
+###################################################################################################
+######################################## GENERAL ALIASES ##########################################
+###################################################################################################
 set-alias 	which 	get-command
 set-alias 	grep 	Select-string
 set-alias 	find	get-childitem
-set-alias 	run 	Invoke-History
+set-alias 	run 	Invoke-History   # usage: run <cmd_id>
 set-alias	hs		get-history
 set-alias	open	invoke-item
 set-alias	c		clear-host
- 
-######################################## Function Aliases ##############################
+
+###################################################################################################
+######################################## Function Aliases #########################################
+###################################################################################################
 function 	ll 			{ Get-ChildItem -Force $args }
 function	less()		{ out-host -paging } #TODO not working <-- needs parameters
 function	ff()		{ param($d); if(!$d) { $d="." }; Invoke-Expression "find $d -name -r" }
@@ -82,8 +89,9 @@ function	ws()		{ change-directory "$HOME/workspace" }
 function	wiki()		{ set-location $HOME/workspace/wiki }
 function	ahs()		{ cat (Get-PSReadlineOption).HistorySavePath }
 
-
-######################################### navigation ###########################################
+###################################################################################################
+######################################### navigation ##############################################
+###################################################################################################
 function 	zamfe()	{ change-directory "$HOME\workspace\zam-frontend\" }
 function 	zaxfe()	{ change-directory "$HOME\workspace\zax-frontend\" }
 function 	zambe()	{ change-directory "$HOME\workspace\zam-backend\" }
@@ -94,11 +102,14 @@ function	..()	{ change-directory ".." }
 function	...()	{ change-directory "../.." }
 function	....()	{ change-directory "../../.." }
 function	work()	{ change-directory "$HOME\workspace\" }
+function	jenkins	{ change-directory "$HOME\workspace\jenkins_workflows\" }
 set-alias	be		zambe
 set-alias	fe		zamfe
 set-alias	bmw		bmwfe
 
-#============================== docker/ZAM specific aliases ============================
+###################################################################################################
+######################################### docker/ZAX specific aliases #############################
+###################################################################################################
 #function	go()	{ $cmd = "mvn"; $prms = "clean install"; write-output "Invoking `"$cmd $prms`""; Invoke-Expression "$cmd $prms" }
 function 	rmai()	{ docker rmi $(docker images -q -a) }
 function	rmallc(){ docker rm -f $(docker ps -a -q) }
@@ -110,22 +121,38 @@ function	scpz	{ param($src, $dest) scp -i $KEYFILE_ZAX $src $USR_ZAX@zax:$dest }
 function	scpzr	{ param($src, $dest) scp -i $KEYFILE_ZAX $USR_ZAX@zax:$src $dest } #copy from zax
 function	slog	{ while ($true){ cat .\target\liberty\wlp\usr\servers\defaultServer\logs\console.log; sleep 15 } }
 function	mpackage{ param ($profile); Invoke-Expression "mvn com.spotify:dockerfile-maven-plugin:build -P $profile"; }
+#--> looks for pattern $p in all files contained in folder $d
 function	grepr	{ param ($d, $p); Get-ChildItem $d -Recurse * | Select-String -Pattern "$p" }
+#--> looks for pattern $p in all files contained in folder $d and gives out only the file location in which the pattern is found
 function	grepro	{ param ($d, $p); Get-ChildItem $d -Recurse * | Select-String -Pattern "$p" | Select-Object -Unique Path }
 
 
-######################################### GIT CMDs ###########################################
-function	gst()	{ git status }
+# some of those aliases only work in combination with git_aliases.ps1 script, where some of the git cmds are specified
+###################################################################################################
+######################################### GIT CMDs ################################################
+###################################################################################################
+function	gst()	{ git status $args }
 set-alias	gs		gst
-function	lg()	{ git lg }
-function	go()	{ git checkout }
-set-alias	gco		go
-function	gsh		{ git show }
-set-alias	gs		gsh
+
+function	glg()	{ git lg $args }
+function	glog()	{ git log1 $args}
+function	glb()	{ git log2 $args }
+
+function	gco()	{ git checkout $args}
+set-alias	go		gco
+
+function	gsh		{ git show $args}
+
 set-alias	got		git
 set-alias	get 	git
 
-######################################### HELPER FUNCTIONS ###########################################
+function	ga()	{ git add $args}
+
+function	gcom()	{ git commit $args }
+
+###################################################################################################
+######################################### HELPER FUNCTIONS ########################################
+###################################################################################################
 function	change-directory() #mit Ausgabe vorher-nachher
 {
 	param($dst)
@@ -134,3 +161,8 @@ function	change-directory() #mit Ausgabe vorher-nachher
 	write-output "$prev --> $dst"
 }
 
+###################################################################################################
+######################################### USEFUL CMDS #############################################
+###################################################################################################
+#Get-ChildItem . -Attributes Directory+Hidden -ErrorAction SilentlyContinue -Include ".git" -Recurse
+#Get-ChildItem . -Attributes Directory+Hidden -ErrorAction SilentlyContinue -Filter ".git" -Recurse
